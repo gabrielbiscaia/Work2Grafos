eabc9a02e1c7ee11686e01f59d0195983302f93d
Gabriel Biscaia <ra118928@uem.br>
2022-04-20 17:40:42 -0300

    adição da função base para colorir

--- Grafo.java
+++ Grafo.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Grafo {
     public Vertice[] vertices;
 
@@ -10,5 +13,24 @@ public class Grafo {
 
     public void addAresta(int u, int v){
         vertices[u].adj.add(vertices[v]);
+    }
+
+    public void buscaLargura() {
+        for (Vertice v : vertices) { //a lista de vértices vai ser percorrido e pintar cada vértice da cor cinza demonstrando que não foi percorrido
+            v.cor = "cinza"; //não percorrido
+        }
+        vertices[0].cor = "branco"; //primeiro vértice da cor branca
+        List<Vertice> prioridade = new LinkedList<>();
+        prioridade.add(vertices[0]);
+        while(!prioridade.isEmpty()) { //Enquanto a lista não for vazia
+            Vertice u = prioridade.remove(0);
+            for (Vertice v : u.adj) {
+                if (v.cor.equals("branco")) { //chamar função pra colorir
+                    v.cor = "cinza";
+                    prioridade.add(v);
+                }
+            }
+            u.cor = "preto";
+        }
     }
 }
