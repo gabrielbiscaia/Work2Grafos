cc6ab1da5fde4ef9bed42bf3df93cbe3b46e8a2c
Gabriel Biscaia <ra118928@uem.br>
2022-04-20 18:12:32 -0300

    recado para ultima função

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Grafo g = new LeitorGrafo().criarGrafo();
+        //adicionar função para imprimir todos os vértices com suas respectivas cores
     }
 }
