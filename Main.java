557ddfb9c3cc430fbbe6fd49fa9d0b1b43c78f01
Gabriel Biscaia <ra118928@uem.br>
2022-04-22 19:43:30 -0300

    adição de funções principais da main

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Grafo g = new LeitorGrafo().criarGrafo();
+        g.buscaLargura();
+        g.imprimeVertices();
         //adicionar função para imprimir todos os vértices com suas respectivas cores
     }
 }
