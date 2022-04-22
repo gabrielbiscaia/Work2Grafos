2753a5de329799d6af410002159340e6fb695bd7
Gabriel Biscaia <ra118928@uem.br>
2022-04-22 20:35:24 -0300

    função main pronta

--- Main.java
+++ Main.java
@@ -3,6 +3,5 @@ public class Main {
         Grafo g = new LeitorGrafo().criarGrafo();
         g.buscaLargura();
         g.imprimeVertices();
-        //adicionar função para imprimir todos os vértices com suas respectivas cores
     }
 }
