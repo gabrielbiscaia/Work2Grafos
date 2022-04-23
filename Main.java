d7a775d78cbd94fc41cf6a259e64c310d5f4f124
Samuel Ferreira Amboni <sfamboni@gmail.com>
2022-04-22 21:43:33 -0300

    A principio completado

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Grafo g = new LeitorGrafo().criarGrafo();
         // g.buscaLargura();
-        g.buscaProfundidade();
+        g.percorrerGrafo();
         g.imprimeVertices();
     }
 }
