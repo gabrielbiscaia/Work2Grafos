3cae11e95054cf991d120995929516e2a485121f
Samuel Ferreira Amboni <sfamboni@gmail.com>
2022-04-24 20:21:47 -0300

    Removida uma váriavel nunca utilizda

--- Vertice.java
+++ Vertice.java
@@ -4,13 +4,11 @@ public class Vertice { //bar
     public int id, grauTotal;
     public String cor; //preto, verde, branco e vermelho.
     public LinkedList<Vertice> adj, pais;
-    public boolean colorido;
 
     public Vertice(int id){
         this.id = id + 1;
         this.grauTotal = 0;
         this.adj = new LinkedList<Vertice>();
         this.pais = new LinkedList<Vertice>();
-        this.colorido = false;
     }
 }
